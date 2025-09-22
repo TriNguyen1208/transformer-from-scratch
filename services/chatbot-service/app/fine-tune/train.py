b88cc837fcb77b22ff8808069c009d582d3dc6af
TriNguyen1208 <ductri0981@gmail.com>
2025-09-22 02:05:12 +0000

    fix: delete in train.py

--- services/chatbot-service/app/fine-tune/train.py
+++ services/chatbot-service/app/fine-tune/train.py
@@ -1,25 +1,11 @@
 from app.utils.processor import Preprocessor
 from app.utils.tokenizer import Tokenizer
 import os
-from app.config.constant import BASE_DIR, FILE_NAME, device, max_iter, learning_rate
+from app.config.constant import BASE_DIR, FILE_NAME, device, max_iter, learning_rate, text_encode
 from app.models.transformer import TransformerModel
 from app.utils.utils import get_batch
 import torch
-#file handler
-dataset_path = os.path.join(BASE_DIR, FILE_NAME)
 
-#Load file
-text = Preprocessor.loadText([dataset_path]) #Load text
-
-#Build the vocab
-tokenizer = Tokenizer()
-tokenizer.buildVocabs(text)
-
-#Calculate vocab size
-vocab_size = len(tokenizer.word2idx)
-
-#encode text
-text_encode = torch.tensor(tokenizer.encode(text), dtype=torch.long)
 
 model = TransformerModel()
 model.to(device)
