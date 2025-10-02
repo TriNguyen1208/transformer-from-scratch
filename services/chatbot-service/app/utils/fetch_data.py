d727540431426cd844202b2b1db2c95d5a6a4637
TriNguyen1208 <ductri0981@gmail.com>
2025-10-02 16:06:28 +0700

    feature: fetch data from url

---
+++ services/chatbot-service/app/utils/fetch_data.py
@@ -0,0 +1,65 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from collections import deque
+
+class FetchData:
+    def get_data_page(url: str, selector: str = "h1, h2, h3, h4, h5, p"):
+        response = requests.get(url)
+        response.raise_for_status()
+        html_content = response.text
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        results = ""
+        for element in soup.select(selector):
+            text = element.get_text(" ", strip=True)
+            text = " ".join(text.split())
+            results += " " + text
+        return results
+    
+    def get_link(url: str, prefix: str, pattern: re.compile):
+        response = requests.get(url)
+        response.raise_for_status()
+        html_content = response.text
+        soup = BeautifulSoup(html_content, 'html.parser')
+        links = [a_tag['href'] for a_tag in soup.find_all('a', href=True) if pattern.search(a_tag['href'])]
+        for i, link in enumerate(links):
+            if link.startswith(('http:/','https:/')):
+                continue
+            else:
+                links[i] = prefix + links[i]
+        return links
+
+    def get_data(
+            url: str, 
+            prefix: str = 'https://thanhnien.vn', 
+            pattern: re.Pattern = re.compile(r'/.*-(\d{18})\.htm$|/(\d{18})\.htm$'), 
+            depth: int = 1
+    ):
+        
+        stack = deque()
+        stack.append((url, 0))
+        visited = {url}
+        text = ""
+        while stack :
+            url, dep = stack.pop()
+            if dep != 0:
+                text += FetchData.get_data_page(url) + '\n'
+            if dep == depth:
+                continue
+            urls_next = FetchData.get_link(url, prefix, pattern)
+
+            for url_next in urls_next:
+                if url_next not in visited:
+                    stack.append((url_next, dep + 1))
+                    visited.add(url_next)
+        return text
+    def write_file(data: str, filename):
+        open(file=filename, mode='a', encoding='utf-8').write(data)
+            
+    # def get_data(root: str, prefix: str, depth: int = 1):
+
+
+# print(FetchData.get_data_page("https://thanhnien.vn/sieu-bao-ragasa-con-manh-hon-ca-yagi-185250922095525435.htm", ""))
+# data = FetchData.get_data("https://vnexpress.net/chu-de/du-bao-thoi-tiet-7989", prefix='https://vnexpress.net', depth=1, pattern=re.compile(r'/.*-(\d{7})\.htm$|/(\d{7})\.htm$'))
+# FetchData.write_file(data, filename='hello.txt')
